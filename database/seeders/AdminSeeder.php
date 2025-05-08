b4351233790d01db38a59a02e9bbbdd9f3b6628f
SuryaRf <ayruzrahmad@gmail.com>
2025-05-08 16:09:24 +0700

    add migrate1

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,16 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('admin')->insert([
+            ['admin_nama' => 'Admin Utama', 'no_telp' => '081234567890'],
+        ]);
     }
 }
